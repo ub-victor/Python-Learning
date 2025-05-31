600f33359e3d6b0cf81944251806b5573a81574f
ub-victor <victoireushindi371@gmail.com>
2025-05-31 15:35:40 +0200

    Initial commit

---
+++ removeElement.py
@@ -0,0 +1,9 @@
+# Remove
+
+'''
+    If you become angry at Fat man and want to remove him from your friend list, you will use the remove() function.
+'''
+
+friends = ['Batman', 'Rat Man', "Fat Man"]
+friends.remove("Fat Man")
+print(friends)

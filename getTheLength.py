f9d7ad3cbc6249ff7ceffa3f9a54bf4951d7e729
ub-victor <victoireushindi371@gmail.com>
2025-05-31 15:25:08 +0200

    Initial commit

--- getTheLength.py
+++ getTheLength.py
@@ -2,5 +2,5 @@
 """Sometimes you need to count the total number of elements in a list.The total number of elements in a list is called thr length of the list.
 To know the length of a list a frinds list, you can use a special keyword called len. This len keyword is the short form of length."""
 friends = ['John', 'Jane', 'Doe']
-count = len('friends')
+count = len(friends)
 print(count)

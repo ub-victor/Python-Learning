d1a55eed34438d93a16930bd89c9d18e847d2480
ub-victor <victoireushindi371@gmail.com>
2025-05-31 15:17:35 +0200

    Initial commit

---
+++ getTheLength.py
@@ -0,0 +1,5 @@
+# Length of a list can be found using the len() function.
+"""Addinng a new element is like appending an element to the list.To add "Goat"  to your friends list, you can use the append() method."""
+friends = ['John', 'Jane', 'Doe']
+count = len('friends')
+print(count)

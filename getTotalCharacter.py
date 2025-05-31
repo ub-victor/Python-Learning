e8302bad390ef1fd7196efa1059497b06ebcfd7f
ub-victor <victoireushindi371@gmail.com>
2025-05-31 15:30:08 +0200

    Initial commit

--- getTotalCharacter.py
+++ getTotalCharacter.py
@@ -1,2 +1,6 @@
-
+'''
+The len() function is used to determine the length of a string or a list. In this case, it's applied to the string "Victoire".
+When len('Victoire') is called, it calculates the number of characters in the string "Victoire", which is 8.
+The variable count stores this value (8), and then print(count) displays 8 to the console. 
+'''
 print(len("Victoire"))

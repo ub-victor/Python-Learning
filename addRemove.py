5a124861e2857299b07b319cae63d81e0f05923b
ub-victor <victoireushindi371@gmail.com>
2025-05-31 15:13:14 +0200

    Initial commit

--- addRemove.py
+++ addRemove.py
@@ -1,3 +1,5 @@
 # Append
 """Addinng a new element is like appending an element to the list.To add "Goat"  to your friends list, you can use the append() method."""
 friends = ['John', 'Jane', 'Doe']
+friends.append('Goat')
+print(friends)

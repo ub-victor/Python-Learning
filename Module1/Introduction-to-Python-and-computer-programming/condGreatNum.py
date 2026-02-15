eb672dd16e41362660d650c3921f2a9e4f95e6f8
ub-victor <victoireushindi371@gmail.com>
2026-02-16 00:05:11 +0200

    condition

--- Module1/Introduction-to-Python-and-computer-programming/condGreatNum.py
+++ Module1/Introduction-to-Python-and-computer-programming/condGreatNum.py
@@ -1,3 +1,10 @@
-number1 = int(input('Enter the first number: '));
-number2 = int(input("Enter the second number"));
+number1 = int(input('Enter the first number: '))
+number2 = int(input("Enter the second number"))
 
+if number1 > number2:
+    larger_number = number1
+else:
+    larger_number= number2
+
+
+print("The larger number is:", larger_number)

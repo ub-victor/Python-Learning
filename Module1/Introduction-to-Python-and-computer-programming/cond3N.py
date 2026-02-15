299ad43f5af30bd9af6a40924b2993fd9da36500
ub-victor <victoireushindi371@gmail.com>
2026-02-16 00:13:18 +0200

    condition

--- Module1/Introduction-to-Python-and-computer-programming/cond3N.py
+++ Module1/Introduction-to-Python-and-computer-programming/cond3N.py
@@ -1,4 +1,4 @@
-number1 = int(input('Enter the first number '))
+number1 = int(input('Enter the first number: '))
 number2 = int(input("Enter the second number: "))
 number3 = int(input("Enter the third number: "))
 

b3a51b5c820b7ac963572ec9a31f43e48e1c24d3
ub-victor <victoireushindi371@gmail.com>
2026-02-16 15:44:03 +0200

    Use of operator

--- Module1/Introduction-to-Python-and-computer-programming/operator.py
+++ Module1/Introduction-to-Python-and-computer-programming/operator.py
@@ -1,7 +1,7 @@
 income =  float(input("Enter the annual income: "));
 
 if income < 85528:
-    tax = income * 0.18 - 556.02 
+    tax = income * 0.18 - 556.02  # the * has the higher priority
 tax = round(tax, 0) # Rounds to the nearest whole number
 print("The tax is:", tax, "thalers")
 

f7aa563f32460d00fed9ee043bb9ed6f8ffa7162
ub-victor <victoireushindi371@gmail.com>
2026-02-16 15:25:40 +0200

    Use of Max

--- Module1/Introduction-to-Python-and-computer-programming/max.py
+++ Module1/Introduction-to-Python-and-computer-programming/max.py
@@ -1,7 +1,9 @@
 # Read three numbers
 
-number = int(input("Enter the first number: "));
-number = int(input("Enter the second number: "));
-number = int(input("Enter the third number: "));
+number1 = int(input("Enter the first number: "));
+number2 = int(input("Enter the second number: "));
+number3 = int(input("Enter the third number: "));
 
+lasgestNumber = max(number1, number2, number3);
 
+print("The lasgest number is: ", lasgestNumber);

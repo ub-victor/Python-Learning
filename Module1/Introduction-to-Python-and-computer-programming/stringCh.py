11b66e2a6501dba84427df49f224bd51636b05b0
ub-victor <victoireushindi371@gmail.com>
2026-02-17 20:47:48 +0200

    string character

--- Module1/Introduction-to-Python-and-computer-programming/stringCh.py
+++ Module1/Introduction-to-Python-and-computer-programming/stringCh.py
@@ -2,3 +2,12 @@ course = 'Python for Beginners'
 
 print(course[0]) # it will output the character at index 0
 print(course[-1]) # index at the last character
+print(course[0:3]) # index to the index-1( exclude) the mentioned Index it will be Pyt
+print(course[:5]) # Python consider the 0 as the starting index , = Pyton
+print(course[:]) # all the course
+
+
+name = "Victoire"
+print(name[1:-1])
+#ictoir
+

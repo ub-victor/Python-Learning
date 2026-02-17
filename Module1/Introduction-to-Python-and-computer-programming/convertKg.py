ef58802c58a590f85a975b65bdf048917d48e62f
ub-victor <victoireushindi371@gmail.com>
2026-02-17 20:32:48 +0200

    convertKg

--- Module1/Introduction-to-Python-and-computer-programming/convertKg.py
+++ Module1/Introduction-to-Python-and-computer-programming/convertKg.py
@@ -1,4 +1,5 @@
 # Ask a user their weight (in pounds), convert it to kilograms and print on the terminal
 
-user_weight = input("Enter your weight")
-print("your weight is ", int(user_weight))
+user_weight = input("Enter your weight(lbs): ")
+weight_kg = int(user_weight) * 0.45
+print("your weight is ", weight_kg)

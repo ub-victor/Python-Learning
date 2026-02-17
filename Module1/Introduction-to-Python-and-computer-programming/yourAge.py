626efcb9af99acf2f23bd1936975231917a11ec2
ub-victor <victoireushindi371@gmail.com>
2026-02-17 20:24:50 +0200

    Your ange

--- Module1/Introduction-to-Python-and-computer-programming/yourAge.py
+++ Module1/Introduction-to-Python-and-computer-programming/yourAge.py
@@ -1,5 +1,5 @@
 birth_year = input("Enter your birth year: ")
 print(type(birth_year))
-age = 2019 - int(birth_year)
-print(type(age))
+age = 2026 - int(birth_year)
+print(type(age)) # it is a build-in f(x),  used to determine the data type (class) of an object at runtime. 
 print(age)

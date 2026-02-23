ec5ee81077f9c8884201984fbe33618149ab9ec5
ub-victor <victoireushindi371@gmail.com>
2026-02-23 19:53:09 +0200

    Formatting String

--- Module1/Introduction-to-Python-and-computer-programming/formatedString.py
+++ Module1/Introduction-to-Python-and-computer-programming/formatedString.py
@@ -1,4 +1,11 @@
+
 first = "Victoire"
 last = "Ushindi"
 
-message = first
+
+message = first +" ["+ last + "] is a coder"
+
+msg = f"{first} [{last}] is a coder" # that is what called formatted String 
+
+print(message)
+print(msg)

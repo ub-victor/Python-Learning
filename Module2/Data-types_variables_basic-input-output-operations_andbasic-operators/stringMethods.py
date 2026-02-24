d5b6de3631fc2e8ff6b1ae411ab8d6e972bc8f99
ub-victor <victoireushindi371@gmail.com>
2026-02-24 08:03:41 +0200

    import numpy as np

--- Module2/Data-types_variables_basic-input-output-operations_andbasic-operators/stringMethods.py
+++ Module2/Data-types_variables_basic-input-output-operations_andbasic-operators/stringMethods.py
@@ -2,5 +2,10 @@ course = 'Python for Beginners'
 
 print("The length of course is ", end= "")
 print(len(course))
+print(course.upper())
+
+# find() method help you to get the index of a character
+print(course.find('y'))
+print(course.find('Beginners'))
 
 # 41:38

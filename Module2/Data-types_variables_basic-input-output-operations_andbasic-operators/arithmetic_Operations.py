7461ae8657b80e01902e46b172f4273785698450
ub-victor <victoireushindi371@gmail.com>
2026-03-01 00:59:13 +0200

    Arithmetic Operation

--- Module2/Data-types_variables_basic-input-output-operations_andbasic-operators/arithmetic_Operations.py
+++ Module2/Data-types_variables_basic-input-output-operations_andbasic-operators/arithmetic_Operations.py
@@ -2,3 +2,15 @@
 
 print(10//3)
 # ** this is for the exponent which is the power
+
+print(3**3)
+
+# Here is the order of operation
+    
+        # - exponentiation 2 ** 3
+        # - Multiplication or division
+        # - Addition or subtraction
+        # But the Parenthesis gives  priority
+   
+
+

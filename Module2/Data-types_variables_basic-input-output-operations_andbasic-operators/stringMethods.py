ce01af0e4678aa9bfd072860c82c6f1829d5fc27
ub-victor <victoireushindi371@gmail.com>
2026-03-01 00:39:44 +0200

    lenth/replace

--- Module2/Data-types_variables_basic-input-output-operations_andbasic-operators/stringMethods.py
+++ Module2/Data-types_variables_basic-input-output-operations_andbasic-operators/stringMethods.py
@@ -9,3 +9,5 @@ print(course.find('y'))
 print(course.find('Beginners'))
 # Replace method
 print(course.replace('Beginners', 'Absolute Beginners'))
+# Check if a sentence have some words
+print("Python" in course) # it will outpout a boolens operation

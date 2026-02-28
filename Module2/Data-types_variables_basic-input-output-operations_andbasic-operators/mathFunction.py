cced39679231ff582553cf4e6dd95c854992e43e
ub-victor <victoireushindi371@gmail.com>
2026-03-01 01:07:57 +0200

    Math Function

--- Module2/Data-types_variables_basic-input-output-operations_andbasic-operators/mathFunction.py
+++ Module2/Data-types_variables_basic-input-output-operations_andbasic-operators/mathFunction.py
@@ -4,4 +4,10 @@ x = 2.9
 print(round(x))
 print(abs(-2.9)) # Ubsolute always return a positive number
 
-print(math.ceil(2.9))
+# function in Python returns the smallest integer greater than or equal to a given number. This is also known as the "ceiling" value, denoted mathematically as 
+# .
+# Valeur entière supérieure la plus proche d'un nombre donné.
+# Example: The ceil of 4.2 is 5. Le plafond de 4,2 est 5.
+print(math.ceil(2.2))
+
+

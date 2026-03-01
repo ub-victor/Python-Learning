0485055ad923ad365671ef06caf4468359bcc520
ub-victor <victoireushindi371@gmail.com>
2026-03-02 00:15:49 +0200

    Conditions

--- Module3/Boolean-values_conditional-execution_loops_lists_and_list-processing_logical-and-bitwise-operations/condition.py
+++ Module3/Boolean-values_conditional-execution_loops_lists_and_list-processing_logical-and-bitwise-operations/condition.py
@@ -3,3 +3,9 @@ hot = True
 if hot :
     print("It's a hot day")
     print("Drink plenty of water")
+elif :
+     print("It's cold day")
+     print("Wear warm clothes")
+else:
+      print("It's a lovely day")
+    

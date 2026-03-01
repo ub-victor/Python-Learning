6b88f4c6bb7b9bf35d933cb265220ac7d505732b
ub-victor <victoireushindi371@gmail.com>
2026-03-02 01:34:20 +0200

    Conditions

--- Module3/Boolean-values_conditional-execution_loops_lists_and_list-processing_logical-and-bitwise-operations/logical_operators.py
+++ Module3/Boolean-values_conditional-execution_loops_lists_and_list-processing_logical-and-bitwise-operations/logical_operators.py
@@ -1,1 +1,9 @@
 # If applicant has high income  And good credit Eligible for loan
+
+has_high_income = False
+has_good_credit = True
+
+if has_high_income and has_good_credit:
+    print("Eligible for loan") 
+if has_high_income and has_good_credit:
+    print("Not  eligible for loan") 

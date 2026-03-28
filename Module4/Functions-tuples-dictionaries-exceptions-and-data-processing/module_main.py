30f466e3a72ba2f10d75421c6ecd8c13864c99e9
ub-victor <victoireushindi371@gmail.com>
2026-03-28 19:31:03 +0200

    module

--- Module4/Functions-tuples-dictionaries-exceptions-and-data-processing/module_main.py
+++ Module4/Functions-tuples-dictionaries-exceptions-and-data-processing/module_main.py
@@ -1,5 +1,10 @@
 import module
+from module import kg_to_lbs, lbs_to_kg
 
-print(module.kg_to_lbs(75))
 
-print(module.lbs_to_kg(75))
+
+# print(module.kg_to_lbs(75))
+# print(module.lbs_to_kg(75))
+
+print(kg_to_lbs(60))
+print(lbs_to_kg(168))

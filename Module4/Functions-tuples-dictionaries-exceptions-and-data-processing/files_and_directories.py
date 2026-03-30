e69870c8026b1c83b20017850fc7b9808f946e5c
ub-victor <victoireushindi371@gmail.com>
2026-03-30 17:52:11 +0200

    File and Directory

--- Module4/Functions-tuples-dictionaries-exceptions-and-data-processing/files_and_directories.py
+++ Module4/Functions-tuples-dictionaries-exceptions-and-data-processing/files_and_directories.py
@@ -1,4 +1,4 @@
 from  pathlib import Path
 
-# Absolute path : is when we look forn a path from the root d
+# Absolute path : is a path from the root of our hard disk
 # Relative path:  is the path start from the current directory

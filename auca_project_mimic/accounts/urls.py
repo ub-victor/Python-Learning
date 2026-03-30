160c331aa790f3ceedf29ae8394b0ab592460381
ub-victor <victoireushindi371@gmail.com>
2026-03-30 11:56:34 +0200

    url implemented

--- auca_project_mimic/accounts/urls.py
+++ auca_project_mimic/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import login_view
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
+    path('', login_view, name='login'),
 ]

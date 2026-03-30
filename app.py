317617d9e5d9a019078344501013be557e69f06c
ub-victor <victoireushindi371@gmail.com>
2026-03-30 18:37:10 +0200

    File and Directory

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from  ecommerce.shipping import calc_shipping
 
 calc_shipping()
 path = Path('ecommerce') # ecommerce folder in the current directory
-print(path.exists())
+create = path("email")
+print(create.mkdir())
 
 
 

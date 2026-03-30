70a5493ac335130424c7438f7286695ff4bd38b4
ub-victor <victoireushindi371@gmail.com>
2026-03-30 19:09:07 +0200

    File and Directory

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from pathlib import Path
 
-from  ecommerce.shipping import calc_shipping
+# from  ecommerce.shipping import calc_shipping
 
-calc_shipping()
-path = Path('ecommerce') # ecommerce folder in the current directory
-create = Path("email")
+# calc_shipping()
+# path = Path('ecommerce') # ecommerce folder in the current directory
+# create = Path("email")
 
 # print(path.exists()) # Check if a folder exist
 
 # print(create.mkdir()) # it will show in the terminal none but the folder will  be created
 # create.rmdir() # to delete a form
+
+path = Path()
+print(path.glob('*.py'))
 
 
 

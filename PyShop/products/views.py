67876115cdfd0fcb6df6d73e76dcf26a2eacc36b
ub-victor <victoireushindi371@gmail.com>
2026-04-20 21:02:30 +0200

    Django

--- PyShop/products/views.py
+++ PyShop/products/views.py
@@ -1,5 +1,6 @@
+from  django.http import HttpResponse
 from django.shortcuts import render
 # django is the package / shortcuts the modules in modules shortcuts with call render
 # Create your views here.
 
-def  index(request, response)
+def  index(request, response):

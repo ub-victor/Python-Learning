f0c36a73c7114d218f93367381b0f4553152f05d
ub-victor <victoireushindi371@gmail.com>
2026-05-02 00:52:14 +0200

    Initial commit

--- PyShop/products/admin.py
+++ PyShop/products/admin.py
@@ -1,2 +1,3 @@
 from django.contrib import admin
-from .models import Product # Import the Product model from the current app's models.py
+# Import the Product model from the current app's models.py
+from .models import Product 

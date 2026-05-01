fc355d7324c08d5041dd6e11ad1f25c041b7faa0
ub-victor <victoireushindi371@gmail.com>
2026-05-01 08:16:08 +0200

    Intial commit

--- PyShop/products/models.py
+++ PyShop/products/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 
 class Product (models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=255)
     price = models.FloatField()
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2083)

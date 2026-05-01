82de95a1186127c7cd70a8e6ff685191d8159303
ub-victor <victoireushindi371@gmail.com>
2026-05-02 00:54:55 +0200

    Initial commit

--- PyShop/products/admin.py
+++ PyShop/products/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 # Import the Product model from the current app's models.py
 from .models import Product 
 
-
+# Register the Product model with the admin site so it can be managed through the Django admin interface
 admin.site.register(Product) 
